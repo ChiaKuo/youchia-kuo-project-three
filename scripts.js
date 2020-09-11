eb7cd7d88152ddd4b590412f41566613bafd8a27
Jason <youchia1991@gmail.com>
2020-09-11 01:04:36 -0400

    Added check function for ace handling and hand value

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 
 
 // Generates arrays for the deck, player's hand, and dealer's hand
-    const suitRef = ['hearts', 'diamonds', 'spades', 'clubs'];
+    const suitRef = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
     const valueRef = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
     const deck = [];
     const playerHand = [];
@@ -33,47 +33,75 @@
 
     // Function to deal cards to the player and dealer
     function dealCards() {
+        // Clears the hands from the previous game
         deck.splice(0, playerHand.length)
         deck.splice(0, dealerHand.length)
         for (let p = 0; p < 2; p++) {
             const playerRandom = Math.floor(Math.random() * (deck.length - 1));
             playerHand.push(deck[playerRandom]);
             deck.splice(playerRandom, 1);
-            console.log(`Dealt a ${playerHand[p].value} of ${playerHand[p].suit} to the player`);
-            console.log(`${deck.length} cards remaining in the deck!`)
+            // console.log(`Dealt a ${playerHand[p].value} of ${playerHand[p].suit} to the player`);
+            // console.log(`${deck.length} cards remaining in the deck!`)
             
         }
         for (let d = 0; d < 2; d++) {
             const dealerRandom = Math.floor(Math.random() * (deck.length - 1));
             dealerHand.push(deck[dealerRandom]);
             deck.splice(dealerRandom, 1);
-            console.log(`Dealt a ${dealerHand[d].value} of ${dealerHand[d].suit} to the dealer`);
-            console.log(`${deck.length} cards remaining in the deck!`)
+            // console.log(`Dealt a ${dealerHand[d].value} of ${dealerHand[d].suit} to the dealer`);
+            // console.log(`${deck.length} cards remaining in the deck!`)
         }
         $('.playerOne').html(`<h2>${playerHand[0].value} of ${playerHand[0].suit}</h2>`)
         $('.playerTwo').html(`<h2>${playerHand[1].value} of ${playerHand[1].suit}</h2>`)
         $('.dealerOne').html(`<h2>${dealerHand[0].value} of ${dealerHand[0].suit}</h2>`)
         $('.dealerTwo').html(`<h2>${dealerHand[1].value} of ${dealerHand[1].suit}</h2>`)
     }
+
+    // Callback function for the game mechanics
+    
+    // Function to check for aces
+    function check(hand) {
+        let handValue = 0;
+        for (let i = 0; i < hand.length; i++) {
+            if (hand[i].value === "Ace") {
+                handValue += 1
+            } else if (hand[i].value === "Jack" || hand[i].value === "Queen" ||         hand[i].value === "King") {
+                handValue += 10
+            } else {
+                handValue += parseInt(hand[i].value);
+            }
+        }
+        for (let i = 0; i < hand.length; i++) {
+            if (hand[i].value.indexOf("Ace") != -1 && (handValue + 10) < 22) {
+                handValue += 10;
+            }
+        }
+        
+        return handValue;
+        
+    }
+
+    // While loop for the game
+
+    // Function to hit the deck
+    function hit(hand) {
+        hand.push(deck[0])
+        deck.splice(0,1)
+        console.log(`Dealt a ${hand[hand.length-1].value} of ${hand[hand.length-1].suit} to the player`);
+    }
+
     generateDeck();
     shuffleDeck();
     dealCards();
-    // console.log(playerHand);
-    // console.log(playerHand);
+    console.log(`Player has ${check(playerHand)}`);
+    console.log(`Dealer has ${check(dealerHand)}`);
+    hit(playerHand);
+    console.log(playerHand);
+    console.log(`Player has ${check(playerHand)}`);
 
-    // $('.playerOne').html(`<h2>${playerHand[0].value} of ${playerHand[0].suit}</h2>`)
-    // $('.playerTwo').html(`<h2>${playerHand[1].value} of ${playerHand[1].suit}</h2>`)
-    // $('.dealerOne').html(`<h2>${dealerHand[0].value} of ${dealerHand[0].suit}</h2>`)
-    // $('.dealerTwo').html(`<h2>${dealerHand[1].value} of ${dealerHand[1].suit}</h2>`)
 
-    // Callback function for the game mechanics
-
-    
-
-    // Function to hit the deck
-    function hit() {
-
-    }
+    // }
+    // blackjackPlay()
 
     // Function to stay
     function stay() {
